9f6acc3f7465a59e744a6c5d33254bfd83d1af02
John Nolan <john.nolan@digital.justice.gov.uk>
2020-05-01 09:33:19 +0100

    fix(component): changed metric value format to just return value

--- src/analyticsTracker.js
+++ src/analyticsTracker.js
@@ -3,9 +3,9 @@ import { log, track } from "./analyticsReporter";
 function AnalyticsTracker(options, debug = false) {
   const { metricName, data } = options;
   if (debug) {
-    log(metricName, { duration: data });
+    log(metricName, data);
   } else {
-    track(metricName, { duration: data });
+    track(metricName, data);
   }
 }
 
