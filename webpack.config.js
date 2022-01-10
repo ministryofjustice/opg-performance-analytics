91db0ecee5eb1ca2954847e11b6f0871ddea3fd4
John Nolan <john.nolan@digital.justice.gov.uk>
2022-01-10 09:25:05 +0000

    build: adds webpack config to build JS output

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,20 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   mode: "production",
+  target: ["web", "es5"],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "lib"),
-    libraryTarget: "commonjs2",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
   },
 };
